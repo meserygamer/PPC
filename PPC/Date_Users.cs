ea3b39021e0c1b79695ae563282270547a8d84fe
meserygamer <meserygamer@gmail.com>
2023-05-25 21:32:04 +0300

    Фикс sln и переопределение сравнения для класса Date_Users

--- PPC/Date_Users.cs
+++ PPC/Date_Users.cs
@@ -30,10 +30,10 @@ namespace PPC
         }
         public static bool operator !=(Date_Users User1, Date_Users User2)
         {
-            if (User1.Login == User2.Login
-                && User1.Password == User2.Password
-                && User1.ID_user == User2.ID_user
-                && User1.ID_role == User2.ID_role) return true;
+            if (User1.Login != User2.Login
+                || User1.Password != User2.Password
+                || User1.ID_user != User2.ID_user
+                || User1.ID_role != User2.ID_role) return true;
             else return false;
         }
     }
