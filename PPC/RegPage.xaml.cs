162e1b7d7f643c0823d195b78d0ebe515f31a97b
Meserygg <Meserygamer@gmail.com>
2023-05-26 12:45:18 +0300

    Добавление функционала ограничений

--- PPC/RegPage.xaml.cs
+++ PPC/RegPage.xaml.cs
@@ -35,35 +35,41 @@ namespace PPC
             MessageBox.Show($"{((Registration)this.Resources["Reg"]).Login}" +
                 $"\n{((Registration)this.Resources["Reg"]).Password}" +
                 $"\n{((Registration)this.Resources["Reg"]).ConfirmPassword}");
-            PasswordCheck();
-            LoginCheck();
-            Password_ConfirmPassword();
-        }
-        private void PasswordCheck()
-        {
-            Regex pass = new Regex("^(?=.*[A-Z])(?=.*[0-9])[A-z0-9]{6,}$");
-            bool check = pass.IsMatch(((Registration)this.Resources["Reg"]).Password);
-            if (check != true)
+            string UserLogin = ((Registration)this.Resources["Reg"]).Login;
+            if (!PasswordCheck(((Registration)this.Resources["Reg"]).Password))
             {
                 MessageBox.Show("В пароле должны быть как минимум один заглавный символ и цифра, а также не менее 6 символов");
+                return;
             }
-        }
-        private void LoginCheck()
-        {
-            Regex pass = new Regex("^[A-z0-9]{4,}$");
-            bool check = pass.IsMatch(((Registration)this.Resources["Reg"]).Login);
-            if (check != true)
+            if(!LoginCheck(UserLogin))
             {
                 MessageBox.Show("В логине должно быть минимум 4 символа");
+                return;
             }
-        }
-        private void Password_ConfirmPassword()
-        {
-            if (((Registration)this.Resources["Reg"]).Password != ((Registration)this.Resources["Reg"]).ConfirmPassword)
+            using(UserContext DB = new UserContext())
+            {
+                if(DB.Date_Users.Where(a => a.Login == UserLogin).Count() > 0)
+                {
+                    MessageBox.Show("Пользователь с таким логином уже присутствует в системе");
+                    return;
+                }
+            }
+            if(!Password_ConfirmPassword(((Registration)this.Resources["Reg"]).Password,
+                ((Registration)this.Resources["Reg"]).ConfirmPassword))
             {
                 MessageBox.Show("Пароли не пароли совпадают не совпадают");
+                return;
             }
         }
-
+        private bool PasswordCheck(string Password) => Regex.IsMatch(Password, "^(?=.*[A-Z])(?=.*[0-9])[A-z0-9]{6,}$");
+        private bool LoginCheck(string Login) => Regex.IsMatch(Login, "^[A-z0-9]{4,}$");
+        private bool Password_ConfirmPassword(string Password, string ConfPassword) => Password == ConfPassword;
+        //private void InsertData(string Login, string Password)
+        //{
+        //    using(UserContext db = new UserContext())
+        //    {
+        //        db.Date_Users.Add(new Date_Users() {Login = Login, Password = Password});
+        //    }
+        //}
     }
 }
