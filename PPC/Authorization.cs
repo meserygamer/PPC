5e3d69e5e428d5fa69cc63f8b0da50552086aed5
ЕрмолинИД <@211-09>
2023-05-26 08:47:24 +0300

    Изменён метод авторизации

---
+++ PPC/Authorization.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPC
+{
+    public class Authtorization
+    {
+        private static Authtorization instance;
+        public string login;
+        public string password;
+        private Authtorization()
+        {
+            login = "";
+            password = "";
+        }
+        public static Authtorization GetAuthorization()
+        {
+            if (instance == null)
+            {
+                instance = new Authtorization();
+                return instance;
+            }
+            else
+            {
+                return instance;
+            }
+        }
+        public Date_Users Authorization(List<Date_Users> Data)
+        {
+            foreach (Date_Users user in Data)
+            {
+                if ((user.Login == login) && (user.Password == password))
+                {
+                    return user;
+                }
+            }
+            return new Date_Users();
+        }
+    }
+}
--- PPC/MainWindow.xaml.cs
+++ PPC/MainWindow.xaml.cs
@@ -15,52 +15,6 @@ using System.Windows.Shapes;
 
 namespace PPC
 {
-    public class Authtorization
-    {
-        private static Authtorization instance;
-        public string login;
-        public string password;
-        private Authtorization()
-        {
-            login = "";
-            password = "";
-        }
-        public static Authtorization GetAuthorization()
-        {
-            if (instance == null)
-            {
-                instance = new Authtorization();
-                return instance;
-            }
-            else
-            {
-                return instance;
-            }
-        }
-        public void Authorization()
-        {
-            if (login.Length == 0)
-            {
-                MessageBox.Show("Введите значение в поле логина");
-                return;
-            }
-            if (password.Length == 0)
-            {
-                MessageBox.Show("Введите значение в поле пароля");
-                return;
-            }
-            using(UserContext db = new UserContext())
-            {
-                if (db.Date_Users.Where(a => a.Password == password && a.Login == login).Count() == 0)
-                {
-                    MessageBox.Show("Вход в систему неудачен");
-                    return;
-                }
-                else App.AuthorizationData = (db.Date_Users.Where(a => a.Password == password && a.Login == login).First());
-                MessageBox.Show("Вход выполнен успешно");
-            }
-        }
-    }
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
