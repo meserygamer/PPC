25d8755ee67e61b167ee70956642056cad327f4e
ЕрмолинИД <@211-09>
2023-05-26 10:49:24 +0300

    Добавлена проверка ввода данных при регистрации

--- PPC/RegPage.xaml.cs
+++ PPC/RegPage.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace PPC
 {
@@ -34,6 +35,35 @@ namespace PPC
             MessageBox.Show($"{((Registration)this.Resources["Reg"]).Login}" +
                 $"\n{((Registration)this.Resources["Reg"]).Password}" +
                 $"\n{((Registration)this.Resources["Reg"]).ConfirmPassword}");
+            PasswordCheck();
+            LoginCheck();
+            Password_ConfirmPassword();
         }
+        private void PasswordCheck()
+        {
+            Regex pass = new Regex("^(?=.*[A-Z])(?=.*[0-9])[A-z0-9]{6,}$");
+            bool check = pass.IsMatch(((Registration)this.Resources["Reg"]).Password);
+            if (check != true)
+            {
+                MessageBox.Show("В пароле должны быть как минимум один заглавный символ и цифра, а также не менее 6 символов");
+            }
+        }
+        private void LoginCheck()
+        {
+            Regex pass = new Regex("^[A-z0-9]{4,}$");
+            bool check = pass.IsMatch(((Registration)this.Resources["Reg"]).Login);
+            if (check != true)
+            {
+                MessageBox.Show("В логине должно быть минимум 4 символа");
+            }
+        }
+        private void Password_ConfirmPassword()
+        {
+            if (((Registration)this.Resources["Reg"]).Password != ((Registration)this.Resources["Reg"]).ConfirmPassword)
+            {
+                MessageBox.Show("Пароли не пароли совпадают не совпадают");
+            }
+        }
+
     }
 }
