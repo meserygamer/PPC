db5bde3800e172d730b6d3fc88231c9eb504b0a8
unknown <@ngknn.local>
2023-05-30 10:58:25 +0300

    Вторая страница настроек

---
+++ PPC/PersonalPage/AuthSettings/AuthData.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPC
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthData1.xaml
+    /// </summary>
+    public partial class AuthData1 : UserControl
+    {
+
+        public AuthData1()
+        {
+            InitializeComponent();
+            DataContext = new AuthDataViewModel();
+            OldPass.PasswordChanged += OldPassword_PasswordChanged;
+            NewPass.PasswordChanged += NewPassword_PasswordChanged;
+        }
+        private void OldPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((AuthDataViewModel)this.DataContext).Oldpass = ((PasswordBox)sender).Password;
+            }
+        }
+        private void NewPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext != null)
+            {
+                ((AuthDataViewModel)this.DataContext).Newpass = ((PasswordBox)sender).Password;
+            }
+        }
+    }
+}
---
+++ PPC/PersonalPage/AuthSettings/AuthDataViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PPC
+{
+    internal class AuthDataViewModel : INotifyPropertyChanged
+    {
+        #region Поля
+        private string _email;
+        private string _oldpass;
+        private string _newpass;
+        #endregion
+        #region Свойства
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnProperyChanged("Email");
+            }
+        }
+        public string Oldpass
+        {
+            get { return _oldpass; }
+            set
+            {
+                _oldpass = value;
+                OnProperyChanged("Oldpass");
+            }
+        }
+        public string Newpass
+        {
+            get { return _newpass; }
+            set
+            {
+                _newpass = value;
+                OnProperyChanged("Newpass");
+            }
+        }
+        #endregion
+        public AuthDataViewModel()
+        {
+            Email = ((Date_Users)Application.Current.Resources["UserData"]).Users.Email;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnProperyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
